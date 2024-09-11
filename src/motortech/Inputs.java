303ab84f149b98679b6fbdb4ed8ce5d309566ec2
Donato Silva <figueroajesus2015@hotmail.com>
2024-09-11 13:00:53 -0500

    add dialogMessageDialog funtion

---
+++ src/motortech/Inputs.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motortech;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+/**
+ *
+ * @author figue
+ */
+public class Inputs {
+
+    
+    public static void inputTextFocus(JTextField input, String startString) {
+        if (input.getText().equals(startString)){
+            input.setText("");
+        }
+        
+        input.setForeground(Color.black);
+    }
+    
+    public static void inputTextFocus(JTextField input , String startString, boolean a) {
+        if (input.getText().equals("")) {
+            input.setText(startString);
+            input.setForeground(MotorTech.getBgGrey());
+        }
+    }
+    
+    public static void dialogMessageDialog(JFrame window, JTextField input, String title, String message) {
+        String[] option = {"¡ Listo !"};
+        
+        JOptionPane.showOptionDialog(window, message, title, 0, JOptionPane.WARNING_MESSAGE, null, option, 0);
+        input.requestFocus();
+    }
+    
+    public static void dialogMessageDialog(JFrame window, JPasswordField input, String title, String message) {
+        String[] option = {"¡ Listo !"};
+        
+        JOptionPane.showOptionDialog(window, message, title, 0, JOptionPane.WARNING_MESSAGE, null, option, 0);
+        input.requestFocus();
+    }
+    
+    public static void inputPassFocus(JPasswordField input, String startString) {
+        if (String.valueOf(input.getPassword()).equals(startString)){
+            input.setText("");
+        }
+        
+        input.setForeground(Color.black);
+    }
+    
+    public static void inputPassFocus(JPasswordField input, String startString, boolean a) {
+        if (String.valueOf(input.getPassword()).equals("")) {
+            input.setText(startString);
+            input.setForeground(MotorTech.getBgGrey());
+        }
+    }
+}
